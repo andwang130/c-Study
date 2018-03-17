0d10b08252b7a07b9a6d4b4d9406134fb79a9ab4
andwang130 <627656470@qq.com>
2018-03-18 01:35:21 +0800

    添加了注释

--- 九九乘法表.cpp
+++ 九九乘法表.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-
+	//for 循环，和其他编程语言一样
 	for (int i = 1; i <= 9; i++) {
 		for (int j = 1; j <= 9; j++) {
 			
@@ -10,11 +10,11 @@ int main() {
 				continue;
 			}
 			cout<<j<< "*" << i << "=" << i * j<<"  ";
-			
+			//count是c++的打印语句
 			
 		}
-		cout << endl;
+		cout << endl;//endl可以打印出一个回车，和清空缓存区的作用
 	}
-	system("pause");
+	system("pause");//暂停控制台语句
 	return 0;
 }
